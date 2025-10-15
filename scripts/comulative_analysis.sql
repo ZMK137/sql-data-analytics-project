e88dbe89a0f7f9f597eba1b095baa8c749c7e216
ZMK137 <szymonziem03@gmail.com>
2025-10-16 00:52:34 +0200

    Add monthly sales and running total calculation
    Added a query to calculate total sales per month along with running totals and average prices.

--- scripts/comulative_analysis.sql
+++ scripts/comulative_analysis.sql
@@ -13,3 +13,17 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY to_char(date_trunc('month', order_date), 'YYYY-MM')
 ORDER BY to_char(date_trunc('month', order_date), 'YYYY-MM');
+
+-- Calculate the total sales per month
+-- and the running total of sales over time 
+
+SELECT
+    to_char(date_trunc('month', order_date), 'YYYY-MM') AS month,
+    SUM(sales_amount) AS total_sales,
+    SUM(SUM(sales_amount)) OVER (ORDER BY date_trunc('month', order_date)) AS running_total_sales,
+    ROUND(AVG(price), 1)  AS avg_price,
+    ROUND(AVG(AVG(price)) OVER (ORDER BY date_trunc('month', order_date)), 1) AS running_avg_price
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY to_char(date_trunc('month', order_date), 'YYYY-MM'), date_trunc('month', order_date)
+ORDER BY to_char(date_trunc('month', order_date), 'YYYY-MM');
